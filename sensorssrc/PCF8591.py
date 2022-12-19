9c8c4f0b12ac5dcc04112751afec47551d2a9bb9
Aggelos Papadakis <theakero@gmail.com>
2022-12-19 22:07:28 +0200

    hm

--- sensorssrc/PCF8591.py
+++ sensorssrc/PCF8591.py
@@ -26,45 +26,45 @@ bus = smbus.SMBus(1)
 
 
 def setup(Addr):
-	global address
-	address = Addr
+    global address
+    address = 18
 
 
 def read(chn):  # channel
-	try:
-		if chn == 0:
-			bus.write_byte(address, 0x40)
-		if chn == 1:
-			bus.write_byte(address, 0x41)
-		if chn == 2:
-			bus.write_byte(address, 0x42)
-		if chn == 3:
-			bus.write_byte(address, 0x43)
-		bus.read_byte(address)  # dummy read to start conversion
-	except Exception as e:
-		print("Address: %s" % address)
-		print(e)
-	return bus.read_byte(address)
+    try:
+        if chn == 0:
+            bus.write_byte(address, 0x40)
+        if chn == 1:
+            bus.write_byte(address, 0x41)
+        if chn == 2:
+            bus.write_byte(address, 0x42)
+        if chn == 3:
+            bus.write_byte(address, 0x43)
+        bus.read_byte(address)  # dummy read to start conversion
+    except Exception as e:
+        print("Address: %s" % address)
+        print(e)
+    return bus.read_byte(address)
 
 
 def write(val):
-	try:
-		temp = val  # move string value to temp
-		temp = int(temp)  # change string to integer
-		# print temp to see on terminal else comment out
-		bus.write_byte_data(address, 0x40, temp)
-	except Exception as e:
-		print("Error: Device address: 0x%2X" % address)
-		print(e)
+    try:
+        temp = val  # move string value to temp
+        temp = int(temp)  # change string to integer
+        # print temp to see on terminal else comment out
+        bus.write_byte_data(address, 0x40, temp)
+    except Exception as e:
+        print("Error: Device address: 0x%2X" % address)
+        print(e)
 
 
 if __name__ == "__main__":
-	setup(0x48)
-	while True:
-		print('AIN0 = ', read(0))
-		print('AIN1 = ', read(1))
-		tmp = read(0)
-		# LED won't light up below 125, so convert '0-255' to '125-255'
-		tmp = tmp*(255-125)/255+125
-		write(tmp)
+    setup(0x48)
+    while True:
+        print('AIN0 = ', read(0))
+        print('AIN1 = ', read(1))
+        tmp = read(0)
+        # LED won't light up below 125, so convert '0-255' to '125-255'
+        tmp = tmp*(255-125)/255+125
+        write(tmp)
 #
