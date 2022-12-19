132305ab6484e14f3694328c25e0682b2712012d
Aggelos Papadakis <theakero@gmail.com>
2022-12-19 21:31:23 +0200

    Fixes

--- sensorssrc/raindropsard.py
+++ sensorssrc/raindropsard.py
@@ -1,4 +1,5 @@
-import PCF8591 as ADC
+import adafruit_pcf8591.pcf8591 as ADC
+# import PCF8591 as ADC
 import RPi.GPIO as GPIO
 import time
 import math
@@ -6,9 +7,11 @@ import math
 DO = 17
 GPIO.setmode(GPIO.BCM)
 
+
 def setup():
     ADC.setup(0x48)
     GPIO.setup(DO, GPIO.IN)
+
 
 def Print(x):
     if x == 1:
@@ -21,17 +24,19 @@ def Print(x):
               ****
               Raining!
               ****''')
-        
+
+
 def loop():
     status = 1
     while True:
-        print (ADC.read(0))
-        
+        print(ADC.read(0))
+
         tmp = GPIO.input(DO)
         if tmp != status:
             Print(tmp)
             status = tmp
         time.sleep(0.2)
-        
+
+
 setup()
 loop()
