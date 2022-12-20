10a70c681a9cc0434378d027111d87dc7d732efd
Aggelos Papadakis <theakero@gmail.com>
2022-12-20 22:41:54 +0200

    asd

--- sensorssrc/yl83ai.py
+++ sensorssrc/yl83ai.py
@@ -14,38 +14,41 @@ rain_level = GPIO.input(rain_pin)
 rain_threshold = 50
 
 # Check if it is raining based on the rain level
-# if rain_level > rain_threshold:
-#     print("It is raining.")
-# else:
-#     print("It is not raining.")
+if rain_level > rain_threshold:
+    print("It is raining.")
+else:
+    print("It is not raining.")
 
 # rain %
-pulse_start = 0
-pulse_end = 0
-while True:
-    if GPIO.input(rain_pin) == 1:
-        pulse_start = time.time()
-    elif GPIO.input(rain_pin) == 0:
-        pulse_end = time.time()
-        break
+# pulse_start = 0
+# pulse_end = 0
+# while True:
+#     if GPIO.input(rain_pin) == 1:
+#         pulse_start = time.time()
+#     elif GPIO.input(rain_pin) == 0:
+#         pulse_end = time.time()
+#         break
 
-# Calculate the rain level using the duty cycle of the PWM signal
-duty_cycle = (pulse_end - pulse_start) / 20
-rain_level = duty_cycle * 100
-print("Rain level:", rain_level, "%")
+# # Calculate the rain level using the duty cycle of the PWM signal
+# duty_cycle = (pulse_end - pulse_start) / 20
+# rain_level = duty_cycle * 100
+# print("Rain level:", rain_level, "%")
 
 # Print the moisture level to the console
-while True:
-    if (rain_level) == True:
-        fun = str(rain_level)
-        print(f'Its raining and the level is { fun}')
-        GPIO.cleanup()
+# while True:
+#     if (rain_level) == True:
+#         fun = str(rain_level)
+#         print(f'Its raining and the level is { fun}')
+#         GPIO.cleanup()
 
-    else:
-        fun = str(rain_level)
-        print(f'Its not raining and the level is { fun} ')
-        GPIO.cleanup()
-GPIO.cleanup()
+#     else:
+#         fun = str(rain_level)
+#         print(f'Its not raining and the level is { fun} ')
+#         GPIO.cleanup()
+# GPIO.cleanup()
+while True:
+    print(rain_level)
+    GPIO.cleanup()
 
 # Clean up the GPIO
 # GPIO.cleanup()
