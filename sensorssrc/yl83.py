c14b369b39bde0acf853dd44755fc8a97ad56f3a
Aggelos Papadakis <theakero@gmail.com>
2022-12-20 11:07:54 +0200

    asd

--- sensorssrc/yl83.py
+++ sensorssrc/yl83.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
 # Set up the GPIO pin for the sensor
-rain_pin = 19
+rain_pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(rain_pin, GPIO.IN)
 
@@ -10,8 +10,11 @@ rain_level = GPIO.input(rain_pin)
 
 # Print the moisture level to the console
 while True:
-    print("Rain level:", rain_level)
-    GPIO.cleanup()
+    if (rain_level) == 1:
+        print("Its raining, rain level is" + rain_level)
+        GPIO.cleanup()
+    else:
+        print("No rain" + rain_level)
 
 # Clean up the GPIO
 # GPIO.cleanup()
