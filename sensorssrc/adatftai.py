3542ba60a50105d54a66e8e3fc52ce12fe2a5a99
Aggelos Papadakis <theakero@gmail.com>
2022-12-22 08:52:08 +0200

    blinka fixes

--- sensorssrc/adatftai.py
+++ sensorssrc/adatftai.py
@@ -1,41 +1,33 @@
-import time
-import Adafruit_GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1351
+import board
+import displayio
+from adafruit_st7789 import ST7789
 
-# Set up the display
-disp = Adafruit_SSD1351.SSD1351_240x240(
-    rst=None, dc=23, spi=SPI.SpiDev(0, 0, max_speed_hz=64000000))
+spi = board.SPI()
+while not spi.try_lock():
+    pass
+spi.configure(baudrate=24000000)  # Configure SPI for 24MHz
+spi.unlock()
+tft_cs = board.D5
+tft_dc = board.D6
 
-# Initialize the display
-disp.begin()
+displayio.release_displays()
+display_bus = displayio.FourWire(
+    spi, command=tft_dc, chip_select=tft_cs, reset=board.D9)
 
-# Clear the display
-disp.clear()
+display = ST7789(display_bus, width=240, height=240, rowstart=80)
 
-# Draw a red circle on the display
-disp.drawCircle(120, 120, 50, Adafruit_SSD1351.RED)
+# Make the display context
+splash = displayio.Group()
+display.show(splash)
 
-# Display the image
-disp.display()
+color_bitmap = displayio.Bitmap(240, 240, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFF0000
 
-# Wait for 5 seconds
-time.sleep(5)
+bg_sprite = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               x=0, y=0)
+splash.append(bg_sprite)
 
-# Clear the display
-disp.clear()
-
-# Display text on the display
-disp.drawText(50, 50, "Hello, World!", Adafruit_SSD1351.WHITE)
-
-# Display the text
-disp.display()
-
-# Wait for 5 seconds
-time.sleep(5)
-
-# Clear the display
-disp.clear()
-
-# Clean up the display
-disp.end()
+while True:
+    pass
