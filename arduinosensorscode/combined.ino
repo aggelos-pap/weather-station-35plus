ae20b19ce33454056bdeaecb60e3bd6df847ad5c
Aggelos Papadakis <theakero@gmail.com>
2023-01-05 11:30:23 +0200

    fixes

--- arduinosensorscode/combined.ino
+++ arduinosensorscode/combined.ino
@@ -12,39 +12,49 @@ Sharp_GP2Y1014AU dustSensor;
 APDS9930 proximitySensor;
 Raindrops rainSensor;
 
-void setup() {
-  tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(ST7735_BLACK);
-  Wire.begin();
-  bme.begin();
-  dustSensor.begin();
-  proximitySensor.begin();
-  rainSensor.begin();
+void setup()
+{
+    tft.initR(INITR_BLACKTAB);
+    tft.fillScreen(ST7735_BLACK);
+    Wire.begin();
+    bme.begin();
+    dustSensor.begin();
+    proximitySensor.begin();
+    rainSensor.begin();
 }
 
-void loop() {
-  // Measure temperature, pressure, and humidity with BME280 sensor
-  float temperature = bme.readTemperature();
-  float pressure = bme.readPressure();
-  float humidity = bme.readHumidity();
+void loop()
+{
+    // Measure temperature, pressure, and humidity with BME280 sensor
+    float temperature = bme.readTemperature();
+    float pressure = bme.readPressure();
+    float humidity = bme.readHumidity();
 
-  // Measure distance with APDS9930 proximity sensor
-  float distance = proximitySensor.getDistance();
+    // Measure distance with APDS9930 proximity sensor
+    float distance = proximitySensor.getDistance();
+    if (distance < 200)
+    { // Increase screen brightness if object is closer than 2 meters
+        tft.setBrightness(255);
+    }
+    else
+    {
+        tft.setBrightness(128);
+    }
 
-  // Check for rain with rain sensor
-  bool isRaining = rainSensor.isRaining();
+    // Check for rain with rain sensor
+    bool isRaining = rainSensor.isRaining();
 
-  // Measure dust particles in air with Sharp GP2Y1014AU sensor
-  float dustLevel = dustSensor.read();
+    // Measure dust particles in air with Sharp GP2Y1014AU sensor
+    float dustLevel = dustSensor.read();
 
-  // Display measurements on Adafruit 1.54 TFT screen
-  tft.setCursor(0, 0);
-  tft.setTextSize(1);
-  tft.println("Temperature: " + String(temperature) + " C");
-  tft.println("Pressure: " + String(pressure) + " hPa");
-  tft.println("Humidity: " + String(humidity) + " %");
-  tft.println("Distance: " + String(distance) + " cm");
-  tft.println("Is Raining: " + String(isRaining));
-  tft.println("Dust Level: " + String(dustLevel));
-  delay(1000);
+    // Display measurements on Adafruit 1.54 TFT screen
+    tft.setCursor(0, 0);
+    tft.setTextSize(1);
+    tft.println("Temperature: " + String(temperature) + " C");
+    tft.println("Pressure: " + String(pressure) + " hPa");
+    tft.println("Humidity: " + String(humidity) + " %");
+    tft.println("Distance: " + String(distance) + " cm");
+    tft.println("Is Raining: " + String(isRaining));
+    tft.println("Dust Level: " + String(dustLevel));
+    delay(1000);
 }
